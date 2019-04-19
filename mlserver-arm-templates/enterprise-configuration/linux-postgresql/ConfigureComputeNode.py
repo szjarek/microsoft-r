d109c4e432f880488504c0f2e67a98ca972b6d57
Jaroslaw Szczegielniak <jszczegielniak@objectivity.co.uk>
2019-04-19 13:53:43 +0200

    First attempt to installing R packages during computeNode setup

--- mlserver-arm-templates/enterprise-configuration/linux-postgresql/ConfigureComputeNode.py
+++ mlserver-arm-templates/enterprise-configuration/linux-postgresql/ConfigureComputeNode.py
@@ -9,6 +9,8 @@ poolInitialSize = sys.argv[1]
 poolMaxSize=sys.argv[2]
 
 computeNodeAppSettingsFilePath = "/opt/microsoft/mlserver/9.3.0/o16n/Microsoft.MLServer.ComputeNode/appsettings.json"
+rPackagesScriptFilename = "install_packages.R"
+
 f = open(computeNodeAppSettingsFilePath, "r")
 jsondata = f.read().decode("utf-8-sig").encode("utf-8").replace("\r\n","")
 data = json.loads(jsondata, object_pairs_hook=OrderedDict)
@@ -20,4 +22,5 @@ f = open(computeNodeAppSettingsFilePath, "w")
 json.dump(data, f, indent=2, sort_keys=False)
 f.close()
 
+os.system("Rscript " + rPackagesScriptFilename)
 os.system("az ml admin node setup --computenode")
---
+++ mlserver-arm-templates/enterprise-configuration/linux-postgresql/install_packages.R
@@ -0,0 +1,4 @@
+install.packages("caret")
+install.packages("mlbench")
+install.packages("randomForest")
+install.packages("GA")
